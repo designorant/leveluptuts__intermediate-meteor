ac604b8bcdd77cb422e95d8c09ac86325d986366
Michał Ordon <michal@tehidea.com>
2015-12-31 11:54:38 +0000

    Mmm, arrow function!

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,7 +1,6 @@
 Template.Recipes.onCreated(function() {
-  var self = this;
-  self.autorun(function() {
-    self.subscribe('recipes');
+  this.autorun(() => {
+    this.subscribe('recipes');
   });
 });
 
